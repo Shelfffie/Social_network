2c2eab94af0363e7669752aa3a1bdb588a9c1036
Polina <zilnovapolina@gmail.com>
2026-05-20 00:06:17 +0300

    feat: add layout to auth

---
+++ frontend/app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import { getMe } from "@/features/auth/actions/get-me";
+import "../globals.css";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const user = await getMe();
+  if (user) redirect("/");
+  return (
+    <html lang="en">
+      <body>
+        <div className="flex flex-row h-screen">
+          <div className="flex flex-col gap-10 w-4/5 justify-center items-center h-full pb-50">
+            <h1 className="text-7xl font-bold">Welcome.</h1>
+            <h2 className="text-3xl">Please sign in</h2>
+          </div>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
