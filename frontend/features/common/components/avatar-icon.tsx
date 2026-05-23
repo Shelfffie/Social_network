8154ebc8fbc14b653e05a4d26e45bc02447f4168
Polina <zilnovapolina@gmail.com>
2026-05-23 15:20:36 +0300

    feat: add avatar icon file checkong for using it in edit profile file to correctly display new load photo

--- frontend/features/common/components/avatar-icon.tsx
+++ frontend/features/common/components/avatar-icon.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useMemo } from "react";
 import defaultPhoto from "../../../public/default-avatar.jpg";
 import Image from "next/image";
 
@@ -5,16 +7,30 @@ export default function AvatarIcon({
   img,
   sizes = "4",
 }: {
-  img?: string | null;
+  img?: File | string | null;
   sizes?: string;
 }) {
-  const imageSrc = img ?? defaultPhoto;
+  const imageSrc = useMemo(() => {
+    if (img instanceof File) {
+      return URL.createObjectURL(img);
+    }
+    if (typeof img === "string" && img.length > 0) {
+      return img;
+    }
+    return defaultPhoto;
+  }, [img]);
+
+  console.log(imageSrc);
+
   return (
     <Image
+      width={100}
+      height={100}
       src={imageSrc}
       alt="avatar"
-      className="rounded-full h-auto w-full min-w-15 aspect-square"
+      className="rounded-full h-auto w-full min-w-15 aspect-square object-cover object-center"
       style={{ maxWidth: `${sizes}rem` }}
+      unoptimized={img instanceof File}
     />
   );
 }
