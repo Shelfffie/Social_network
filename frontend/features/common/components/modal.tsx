61a384f07140c1a979dbb1c0e7df95c44172d896
Polina <zilnovapolina@gmail.com>
2026-05-23 15:23:51 +0300

    feat: change modal window component, add background dimming, make x button functional and change common styles

--- frontend/features/common/components/modal.tsx
+++ frontend/features/common/components/modal.tsx
@@ -1,13 +1,24 @@
+"use client";
 import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 import React from "react";
 
 export function Modal({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   return (
     <>
-      <div className="fixed inset-0 z-[999] bg-black/50" />
-      <div className="fixed inset-0 z-[1000] flex items-center justify-center">
-        <X className="hover:text-indigo-600 active:scale-110" />
-        {children}
+      <div className=" flex items-center justify-center fixed inset-0 z-[999] bg-black/50">
+        <div className="relative min-w-90 min-h-135 max-w-300 border-2 rounded-xl shadow-1 bg-indigo-50 overflow-hidden @container">
+          <button
+            className="absolute z-1000 top-4 right-4 hover:text-indigo-600 active:scale-110 transition-all"
+            onClick={() => router.back()}
+          >
+            <X />
+          </button>
+
+          {children}
+        </div>
       </div>
     </>
   );
