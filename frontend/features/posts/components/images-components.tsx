bf929af7e2f2271e4cc981cb58e88949cb09788b
Polina <zilnovapolina@gmail.com>
2026-05-27 19:47:17 +0300

    fix: change style, delete unnecessary styles

--- frontend/features/posts/components/images-components.tsx
+++ frontend/features/posts/components/images-components.tsx
@@ -13,28 +13,10 @@ function ImageItem({
 
   if (!preview) return null;
 
-  if (isCreating) {
-    return (
-      <div className="relative w-24 h-24">
-        <img
-          className="w-full h-full rounded-lg object-cover"
-          src={preview}
-          alt="post image"
-        />
-      </div>
-    );
-  }
-
   return (
-    <div
-      className={`relative ${
-        isSingle ? "aspect-video max-w-[900px]" : "aspect-square"
-      }`}
-    >
+    <div className={`relative aspect-square ${isCreating && " w-24 h-24"}`}>
       <img
-        className={`w-full h-full rounded-lg ${
-          isSingle ? "max-h-[1000px] object-contain" : "object-cover"
-        }`}
+        className="w-full h-full rounded-lg object-cover"
         src={preview}
         alt="post image"
       />
@@ -50,16 +32,16 @@ export default function ImagesComponent({
   isCreating?: boolean;
 }) {
   return (
-    <div className="flex justify-center w-full">
+    <div className="flex justify-center">
       <div
         className={
           isCreating
-            ? "flex items-center justify-center gap-3 overflow-x-auto p-5 w-full"
+            ? "flex justify-items-center gap-1 overflow-x-auto p-5"
             : `grid ${
                 photos.length === 1
                   ? "grid-cols-1"
-                  : "grid-cols-[repeat(auto-fill,minmax(200px,1fr))]"
-              } justify-items-center gap-5 p-5 w-full max-w-[800px]`
+                  : "grid-cols-[repeat(auto-fill,minmax(100px,1fr))]"
+              } justify-items-center gap-2 p-5`
         }
       >
         {photos.map((img, index) => (
