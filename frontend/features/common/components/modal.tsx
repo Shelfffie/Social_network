767e978d8907928a17105f6740c4950d1637b729
Polina <zilnovapolina@gmail.com>
2026-05-27 19:46:31 +0300

    fix: change styles in modal to make it more universal

--- frontend/features/common/components/modal.tsx
+++ frontend/features/common/components/modal.tsx
@@ -4,12 +4,22 @@ import { useRouter } from "next/navigation";
 
 import React from "react";
 
-export function Modal({ children }: { children: React.ReactNode }) {
+export function Modal({
+  children,
+  isPost = false,
+}: {
+  children: React.ReactNode;
+  isPost?: boolean;
+}) {
   const router = useRouter();
   return (
     <>
       <div className=" flex items-center justify-center fixed inset-0 z-[999] bg-black/50">
-        <div className="relative min-w-90 min-h-135 max-w-300 border-2 rounded-xl shadow-1 bg-indigo-50 overflow-hidden @container">
+        <div
+          className={`relative max-h-[93vh] max-w-3xl border-2 rounded-xl shadow-1 bg-indigo-50 overflow-hidden @container ${
+            isPost ? `w-full` : `min-w-90`
+          }`}
+        >
           <button
             className="absolute z-1000 top-4 right-4 hover:text-indigo-600 active:scale-110 transition-all"
             onClick={() => router.back()}
