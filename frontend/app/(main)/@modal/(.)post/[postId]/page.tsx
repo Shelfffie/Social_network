d8a47b09c223d0734cf95bf51ffe9645eb4702a7
Polina <zilnovapolina@gmail.com>
2026-05-29 18:52:21 +0300

    feat: make modal to single post

--- frontend/app/(main)/@modal/(.)post/[postId]/page.tsx
+++ frontend/app/(main)/@modal/(.)post/[postId]/page.tsx
@@ -1,3 +1,6 @@
+import { getCommentsByPost } from "@/features/comments/actions/get-comments";
+import CommentsList from "@/features/comments/components/comments-list";
+import CreateCommentComponent from "@/features/comments/components/create-comment";
 import { Modal } from "@/features/common/components/modal";
 import { getSinglePost } from "@/features/posts/actions/get-posts";
 import PostPageClientWrapper from "@/features/posts/components/single-post-wrapper";
@@ -9,13 +12,21 @@ export default async function PostPage({
 }) {
   const postId = (await params).postId;
   const post = await getSinglePost(postId);
+  const comments: CommentType[] = await getCommentsByPost(postId, 1);
 
   return (
     <Modal isPost={true}>
       <div className="w-full @container">
         <PostPageClientWrapper post={post} />
         <div className="flex justify-center">
-          <h1>COMMENTS IN DEVELOPMENT...</h1>
+          <div className="">
+            <div className="w-full p-2 bg-indigo-50 border-b-1 border-indigo-300">
+              <div className="m-auto w-11/12 pt-10">
+                <CreateCommentComponent postId={postId} />
+              </div>
+            </div>
+            <CommentsList comments={comments} />
+          </div>
         </div>
       </div>
     </Modal>
