ddb35405727d346e8f1baf5f6ae1a4a2e080bbd4
Polina <zilnovapolina@gmail.com>
2026-06-01 22:40:56 +0300

    feat: change drop-down-menu, add actions to delete button

--- frontend/features/posts/components/drop-down-menu-post.tsx
+++ frontend/features/posts/components/drop-down-menu-post.tsx
@@ -9,15 +9,19 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useAuth } from "@/features/auth/contexts/auth-context";
+import { Modal } from "@/features/common/components/modal";
+import WhiteButton from "@/features/common/components/white-indigo-button";
 import { PostType } from "@/features/utils/types/posts/post-type";
 import { EllipsisVertical } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FetchDeletePost } from "../actions/delete-post";
 
 export default function PostDropDownDetails({ post }: { post: PostType }) {
   const { user } = useAuth();
   const router = useRouter();
   const [isAuthour, setIsAuthour] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
     console.log("POST CREATOR");
@@ -30,36 +34,79 @@ export default function PostDropDownDetails({ post }: { post: PostType }) {
     }
   }, [user, post]);
 
+  const handleDeletePost = async (postId: string) => {
+    try {
+      const result = await FetchDeletePost(postId);
+      if (result?.success) {
+        return true;
+      } else {
+        console.log(result?.message);
+        return false;
+      }
+    } catch (error) {
+      return false;
+    } finally {
+      setIsModalOpen(false);
+    }
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost">
-          <EllipsisVertical />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        className="w-40 z-[99999]"
-        align="start"
-        onClick={(e) => e.stopPropagation()}
-      >
-        {isAuthour ? (
-          <>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost">
+            <EllipsisVertical />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className="w-40 z-[99999]"
+          align="start"
+          onClick={(e) => e.stopPropagation()}
+        >
+          {isAuthour ? (
+            <>
+              <DropdownMenuGroup>
+                <DropdownMenuItem
+                  onClick={() => router.push(`/post/${post._id}/edit`)}
+                >
+                  Редагувати пост
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setIsModalOpen(true)}>
+                  Видалити пост
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </>
+          ) : (
             <DropdownMenuGroup>
-              <DropdownMenuItem
-                onClick={() => router.push(`/post/${post._id}/edit`)}
-              >
-                Редагувати пост
-              </DropdownMenuItem>
-              <DropdownMenuItem>Видалити пост</DropdownMenuItem>
+              <DropdownMenuItem>Додати користувача в друзі</DropdownMenuItem>
+              <DropdownMenuItem>Перейти в профіль користувача</DropdownMenuItem>
             </DropdownMenuGroup>
-          </>
-        ) : (
-          <DropdownMenuGroup>
-            <DropdownMenuItem>Додати користувача в друзі</DropdownMenuItem>
-            <DropdownMenuItem>Перейти в профіль користувача</DropdownMenuItem>
-          </DropdownMenuGroup>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)}>
+          <div className="flex flex-col items-center justify-center gap-7 h-50 w-full overflow-hidden">
+            <h1 className="text-lg">
+              Are you sure you want to delete this post?
+            </h1>
+            <p>This action cannot be undone</p>
+            <div className="flex flex-row gap-10">
+              <WhiteButton
+                text="Cancel"
+                onClick={() => setIsModalOpen(false)}
+              />
+              <Button
+                variant="destructive"
+                onClick={() => handleDeletePost(post._id)}
+                className="w-30"
+              >
+                Delete
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      )}
+    </>
   );
 }
