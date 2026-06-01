e42a4db0440a4c31862a5701a16cc5a6a70a0da5
Polina <zilnovapolina@gmail.com>
2026-06-01 22:45:01 +0300

    feat: change modal to make it more universal

--- frontend/features/common/components/modal.tsx
+++ frontend/features/common/components/modal.tsx
@@ -2,22 +2,32 @@
 import { X } from "lucide-react";
 import { useRouter } from "next/navigation";
 
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import ScrollToTopButton from "./scroll-to-top-button";
 
 export function Modal({
   children,
   isPost = false,
+  onClose,
 }: {
   children: React.ReactNode;
   isPost?: boolean;
+  onClose: () => void;
 }) {
   const router = useRouter();
   const modalWindowRef = useRef<HTMLDivElement | null>(null);
 
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
+
   const handleBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
     if (e.target === e.currentTarget) {
-      router.back();
+      onClose();
     }
   };
 
@@ -28,14 +38,15 @@ export function Modal({
         onClick={handleBackgroundClick}
       >
         <div
-          className={`relative max-h-[93vh] max-w-3xl border-2 rounded-xl shadow-1 bg-indigo-50 overflow-y-auto @container ${
+          className={`relative max-h-[93vh] max-w-3xl border-2 rounded-xl shadow-1 bg-indigo-50 overflow-y-auto overflow-x-hidden @container ${
             isPost ? `w-full` : `min-w-90`
           }`}
           ref={modalWindowRef}
+          onClick={(e) => e.stopPropagation()}
         >
           <button
             className="absolute z-1000 top-4 right-4 hover:text-indigo-600 active:scale-110 transition-all"
-            onClick={() => router.back()}
+            onClick={() => onClose()}
           >
             <X />
           </button>
